46d93180204455827fe436da1573be284079398e
newkayak12 <newkayak12@gmail.com>
2022-06-13 22:43:30 +0900

    Feat: add Testcase

--- src/test/java/com/server/base/BaseTest.java
+++ src/test/java/com/server/base/BaseTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Objects;
 
 @SpringBootTest
 @ActiveProfiles(value = "local")
@@ -25,8 +26,8 @@ public class BaseTest {
     ObjectMapper objectMapper;
     HttpHeaders httpHeaders;
     RestTemplate rt;
-    String refresh = "Bearer eyJ0eXAiOiJyZiIsImFsZyI6IkhTNTEyIn0.eyJ1c2VyTm8iOjEsImlzcyI6IlNQUklOR19CQVNFIiwiaWF0IjoxNjU1MDIzNzcyfQ.AfPvdWWvqvwSXqX84fmX6d5yPpkFXmw4Xc489FZ-soyNm-qa7RC9LCUTN1nZf1_PDQCJvx5yPsjqFVpiZtYzRg";
-    String authorization =  "Bearer eyJ0eXAiOiJhYyIsImFsZyI6IkhTNTEyIn0.eyJ1c2VyTnVtIjoiMDEwMTIzNDEyMzQiLCJ1c2VyU3RhdHVzIjoiQUNUSVZBVEVEIiwidXNlck5vIjoxLCJpc3MiOiJTUFJJTkdfQkFTRSIsInVzZXJOYW1lIjoidGVzdDEiLCJleHAiOjE2NTUwNjY5NzIsInVzZXJJZCI6InRlc3QxMiIsImlhdCI6MTY1NTAyMzc3Mn0.OaQvvV6MqafItVNsqZuDNkDRnbR9cUHmyoGN7TE6QVRntF0DMIfd5TKCVyJHXXwTMClic_jDF4Sllzb76YRVsw";
+    String refresh = "Bearer eyJ0eXAiOiJyZiIsImFsZyI6IkhTNTEyIn0.eyJ1c2VyTm8iOjEsImlzcyI6IlNQUklOR19CQVNFIiwiaWF0IjoxNjU1MTIxNDc4fQ.RwaQnk_G9cCzjwr0DgbEWyMCow5TVatCCwfPoUVGmuJ3nHBmOCxzF_UZHd-X_eWcTIWwN1GhOejOQTda8O7tOw";
+    String authorization =  "Bearer eyJ0eXAiOiJhYyIsImFsZyI6IkhTNTEyIn0.eyJ1c2VyTnVtIjoiMDEwMTIzNDEyMzQiLCJ1c2VyU3RhdHVzIjoiQUNUSVZBVEVEIiwidXNlck5vIjoxLCJpc3MiOiJTUFJJTkdfQkFTRSIsInVzZXJOYW1lIjoidGVzdDEiLCJleHAiOjE2NTUxNjQ2NzksInVzZXJJZCI6InRlc3QxMiIsImlhdCI6MTY1NTEyMTQ3OX0.NVzW-N8LPxiCmm3i5ujnkM26Q4K0BUA38rmY7Zk_FUO-zniqFfwtZmp8fft__PShPruTYjC0iPJMvQOzaW-Lvw";
     String prefix = "http://localhost:8080/api/";
 
     @BeforeEach
@@ -45,7 +46,9 @@ public class BaseTest {
             Field[] fields = object.getClass().getDeclaredFields();
             for(Field field : fields){
                 field.setAccessible(true);
-                builder.queryParam(field.getName(), field.get(object));
+                if(!Objects.isNull(field.get(object))){
+                    builder.queryParam(field.getName(), field.get(object));
+                }
             }
             String uri = builder.toUriString();
             String json = objectMapper.writeValueAsString(object);
@@ -87,14 +90,26 @@ public class BaseTest {
     }
     void delete(String url, Object object){
         try {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(prefix+url);
+
+            Field[] fields = object.getClass().getDeclaredFields();
+            for(Field field : fields){
+                field.setAccessible(true);
+                if(!Objects.isNull(field.get(object))){
+                    builder.queryParam(field.getName(), field.get(object));
+                }
+            }
+            String uri = builder.toUriString();
             String json = objectMapper.writeValueAsString(object);
             log.error("JSON!!!::{}", json);
             HttpEntity<String> entity = new HttpEntity<>(json, httpHeaders);
-            ResponseEntity<Map> response = rt.exchange(prefix+url, HttpMethod.DELETE, entity, Map.class);
+            ResponseEntity<Map> response = rt.exchange(uri, HttpMethod.DELETE, entity, Map.class);
             log.warn("[  HEADER  ]  ::: {}", response.getHeaders());
             log.error("\n\n");
             log.warn("[  BODY  ]  ::: {}", response.getBody());
         } catch (JsonProcessingException e) {
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/test/java/com/server/base/UserTests.java
+++ src/test/java/com/server/base/UserTests.java
@@ -1,21 +1,8 @@
 package com.server.base;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.server.base.common.constants.Constants;
 import com.server.base.repository.dto.UserDto;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.lang.reflect.Field;
-import java.util.Map;
 
 
 @Slf4j
@@ -40,14 +27,14 @@ class UserTests extends BaseTest {
         get("/user/signIn", v);
     }
     @Test
-    void subPasswordSignIn(){
+    void subPasswordSignUp(){
         UserDto v = new UserDto();
         v.setUserId("test12");
         v.setPasswordSub("0626");
         patch("/user/easySignUp", v);
     }
     @Test
-    void subPasswordSign(){
+    void subPasswordSignIn(){
         UserDto v = new UserDto();
         v.setPasswordSub("0626");
         get("/user/easySignIn", v);
